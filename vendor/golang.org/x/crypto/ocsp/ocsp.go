65c02be89623b33e8d72167b97eda3ffc454ecb1
David Bensoussan <david.bensoussan@bender.de>
2025-07-11 12:16:14 +0200

    test

--- vendor/golang.org/x/crypto/ocsp/ocsp.go
+++ vendor/golang.org/x/crypto/ocsp/ocsp.go
@@ -181,7 +181,7 @@ var signatureAlgorithmDetails = []struct {
 	{x509.ECDSAWithSHA256, oidSignatureECDSAWithSHA256, x509.ECDSA, crypto.SHA256},
 	{x509.ECDSAWithSHA384, oidSignatureECDSAWithSHA384, x509.ECDSA, crypto.SHA384},
 	{x509.ECDSAWithSHA512, oidSignatureECDSAWithSHA512, x509.ECDSA, crypto.SHA512},
-	{x509.PureEd25519, oidSignatureEd25519, x509.Ed25519, crypto.Hash(0)},
+	{x509.PureEd25519, oidSignatureEd25519, x509.Ed25519, crypto.Hash(0) /* no pre-hashing */},
 }
 
 // TODO(rlb): This is also from crypto/x509, so same comment as AGL's below
@@ -221,7 +221,7 @@ func signingParamsForPublicKey(pub interface{}, requestedSigAlgo x509.SignatureA
 		// Do not set sigAlgo.Parameters — RFC 8410 says it MUST be absent
 
 	default:
-		err = errors.New("x509: only RSA, ECDSA and EdDSA keys supported")
+		err = errors.New("x509: only RSA, ECDSA, and Ed25519 keys supported")
 	}
 
 	if err != nil {
@@ -240,7 +240,8 @@ func signingParamsForPublicKey(pub interface{}, requestedSigAlgo x509.SignatureA
 				return
 			}
 			sigAlgo.Algorithm, hashFunc = details.oid, details.hash
-			if hashFunc == 0 {
+			// EdDSA is special: hashFunc can be 0
+			if details.hash == 0 && requestedSigAlgo != x509.PureEd25519 {
 				err = errors.New("x509: cannot sign with hash function requested")
 				return
 			}
@@ -767,9 +768,18 @@ func CreateResponse(issuer, responderCert *x509.Certificate, template Response, 
 		return nil, err
 	}
 
-	responseHash := hashFunc.New()
-	responseHash.Write(tbsResponseDataDER)
-	signature, err := priv.Sign(rand.Reader, responseHash.Sum(nil), hashFunc)
+	var digest []byte
+	if hashFunc == crypto.Hash(0) {
+		// Ed25519 and similar: sign raw DER
+		digest = tbsResponseDataDER
+	} else {
+		h := hashFunc.New()
+		h.Write(tbsResponseDataDER)
+		digest = h.Sum(nil)
+	}
+
+	signature, err := priv.Sign(rand.Reader, digest, hashFunc)
+
 	if err != nil {
 		return nil, err
 	}
